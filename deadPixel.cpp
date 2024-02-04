89555907a5db0351a060b65aa9684c062834b5e3
Caroline Carvalho <103433489+carolineccarvalho@users.noreply.github.com>
2024-02-04 01:17:49 -0300

    Update deadPixel.cpp

--- deadPixel.cpp
+++ deadPixel.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
-
+ 
 using namespace std;
-
+ 
 void replacement(int &bigger, int aux){
     if(bigger<aux){
         bigger=aux;
@@ -15,7 +15,7 @@ int main()
     int a,b,x,y;
     cin>> tests;
     int bigger[tests], aux;
-
+ 
     for(int i=0; i<tests; i++){
         cin>>a>>b>>x>>y;
         bigger[i] = -1;
@@ -23,19 +23,19 @@ int main()
         aux = (a-1-x)*b;
         replacement(bigger[i],aux);
     }
-    if(x-1>0){
-        aux = (a-x+1)*b;
+    if(x-1>=0){
+        aux = (x)*b;
         replacement(bigger[i],aux);
     }
     if(y+1<b){
         aux = (b-y-1)*a;
         replacement(bigger[i],aux);
     }
-    if(y-1>0){
+    if(y-1>=0){
         aux = (y)*a;
         replacement(bigger[i],aux);
     }}
-
+ 
     for(int i=0;i<tests;i++){
         cout << bigger[i] <<endl;
     }
