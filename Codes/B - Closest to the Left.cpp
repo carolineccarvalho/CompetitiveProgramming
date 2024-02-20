b7e50d4b1fa72178d039596c5f7dd8a34b53f9c4
Caroline Carvalho <103433489+carolineccarvalho@users.noreply.github.com>
2024-02-20 19:14:05 -0300

    Create B - Closest to the Left.cpp

---
+++ Codes/B - Closest to the Left.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int n, k, x;
+    cin >> n;
+    cin >> k;
+    vector<int> elements(n);
+
+    for(int i=0; i<n; i++){
+        cin >> elements[i];
+    }
+
+    for(int i=0; i<k; i++){
+        int l=-1;
+        int r=n;
+        cin >> x;
+        while(r>l+1){
+            int m;
+            m = (l+r)/2;
+            if(elements[m]<=x){
+                l = m;
+            }else{
+                r = m;
+            }
+        }
+
+        cout << l + 1 << endl;
+    }
+}
