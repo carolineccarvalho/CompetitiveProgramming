0a4790ee6d4ef7305fb1a4f9faa9e44058a87503
Caroline Carvalho <103433489+carolineccarvalho@users.noreply.github.com>
2024-02-21 13:29:52 -0300

    Create A. Packing Rectangles.cpp

---
+++ A. Packing Rectangles.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+long long w, h, n;
+
+bool good(long long x){
+    return (x/w)*(x/h) >= n;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> w >> h >> n;
+    long long l=0;
+    long long r=1;
+    while(!good(r)) r*=2;
+    while(r>l+1){
+        long long m = (l+r)/2;
+        if(good(m)){
+            r = m;
+        }else{
+            l = m;
+        }
+    }
+    cout << r << "\n";
+}
