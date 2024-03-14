91d82467de2e1823110ff1f584c34900cfa70769
Caroline Carvalho <103433489+carolineccarvalho@users.noreply.github.com>
2024-03-14 14:43:59 -0300

    Create Topological Sorting.cpp

---
+++ Codes/Topological Sorting.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<set<int>> adj;
+vector<int> color;
+vector<bool> visited;
+vector<int> ans;
+int n, m;
+
+bool eh_ciclo(int v){
+    color[v] = 1;
+    for(auto w: adj[v]){
+        if(color[w] == 0){
+            if(eh_ciclo(w)){
+                return true;
+            }
+        }else if(color[w]==1){
+            return true;
+        }
+    }
+    color[v] = 2;
+    return false;
+}
+
+void dfs(int v){
+    visited[v] = true;
+    for(auto w: adj[v]){
+        if(!visited[w]){
+            dfs(w);
+        }
+    }
+    ans.push_back(v+1);
+}
+
+void toposort(){
+    visited.resize(n);
+    visited.assign(n,false);
+    for(int i=n-1; i>=0; i--){
+        if(!visited[i]){
+            dfs(i);
+        }
+    }
+
+    reverse(ans.begin(), ans.end());
+}
+
+
+int main()
+{
+    int x, y;
+    cin >> n >> m;
+    adj.resize(n);
+    color.resize(n);
+    color.assign(n, 0);
+
+
+
+    for(int i =0; i<m; i++){
+        cin >> x >> y;
+        x--;y--;
+        adj[x].insert(y);
+    }
+
+    for(int i=0; i<n; i++){
+        if(color[i] == 0 && eh_ciclo(i)){
+            cout << i << endl;
+            cout << "Sandro fails." << endl;
+            return 0;
+        }
+    }
+
+
+    toposort();
+
+    for(auto it: ans){
+        cout << it << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
